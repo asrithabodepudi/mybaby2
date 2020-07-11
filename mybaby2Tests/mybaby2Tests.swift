9870f90ed9f6d4f7bb2a613d795ffa7eb6521029
Vidushi <meelvidushi1@gmail.com>
2020-07-11 17:45:08 -0400

    Initial Commit

---
+++ mybaby2Tests/mybaby2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  mybaby2Tests.swift
+//  mybaby2Tests
+//
+//  Created by Vidushi Meel on 7/11/20.
+//
+
+import XCTest
+@testable import mybaby2
+
+class mybaby2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
